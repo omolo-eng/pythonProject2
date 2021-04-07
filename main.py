1a74970c145e5870814584dcc670c7c4b673a17f
Identity <billyomolo76@gmail.com>
2021-04-07 23:17:38 +0300

    Version 101

---
+++ main.py
@@ -0,0 +1,40 @@
+# print ("Hello World")
+
+str = "The sum is: "
+
+
+# print(a%b)
+# The following functions adds two integer variables
+# has no parameters
+def adder():
+    a = 5
+    b = 2
+    print(str, a + b)
+
+
+# Has parameters
+def adder2(a, b):
+    print(str, a - b)
+
+
+print("Hello Again")
+print("i like typing this")
+print("This is fun")
+print('Yay? Printing')
+print("I'd much rather you 'not' .")
+print('I "said" do not touch this.')
+print("I will now count my chickens")
+print("Hens", 25 + 30 / 6)
+print("Roosters", 100 - 25 * 3 % 4)
+print("Now I will count the eggs:")
+print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
+print("Is it true that3+2<5-7?")
+print(3 + 2 < 5 - 7)
+print("What is 3+2?", 3 + 2)
+print("What is 5-7?", 5 - 7)
+print("Oh, that's why it's false.")
+print("How about some more.")
+print("Is it greater?", 5 > -2)
+print("Is it less or equal?", 5 <= -2)
+adder()
+adder2(2, 5)
